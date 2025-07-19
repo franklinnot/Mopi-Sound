9e3f73a07a1f7c1c304a72d2d26c6f82b439cad4
franklinnot <franklinnot@outlook.com>
2025-07-19 04:47:57 -0500

    feat: integrate video ID retrieval and YouTube embed functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,32 @@
 import { useState } from "react";
 import { FiRefreshCw } from "react-icons/fi";
+import getVideoId from "get-video-id";
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
 function App() {
-  const [data, setData] = useState({ link: "", title: "" });
+  const [data, setData] = useState({ link: "", title: "", id: "" });
   const [showDetails, setShowDetails] = useState(false);
 
   const handleNext = () => {
-    if (data.link.trim()) {
-      setShowDetails(true);
+    const video = getVideoId(data.link);
+    if (!video.id || video.service !== "youtube") {
+      alert("Por favor, ingresa un enlace de YouTube válido.");
+      return;
     }
+    setData({ ...data, id: video.id });
+    setShowDetails(true);
   };
 
   return (
     <main
-      className="min-h-dvh grid place-items-center max-sm:pb-40 
-                max-sm:px-8 sm:pb-44 md:pb-48 lg:pb-56"
+      className={`min-h-dvh grid place-items-center max-sm:px-8 py-12 ${
+        !showDetails && "max-sm:pb-40 sm:pb-44 md:pb-48 lg:pb-56"
+      }`}
     >
       <div className="w-full max-w-xl flex flex-col items-center gap-8">
         <img
-          className="w-56 hover-glow mb-5"
+          className={`hover-glow mb-6 ${showDetails ? "w-44" : "w-56"}`}
           src="./icon.svg"
           alt="Logo de MOPI"
         />
@@ -38,7 +46,9 @@ function App() {
               name="link"
               placeholder="https://www.youtube.com/..."
               value={data.link}
-              onChange={(e) => setData({ ...data, link: e.target.value })}
+              onChange={(e) => {
+                setData({ ...data, link: e.target.value.trim() });
+              }}
               className="flex-grow"
             />
             <button
@@ -52,23 +62,21 @@ function App() {
         </div>
 
         {showDetails && (
-          <div className="w-full relative">
-            <div className="w-full flex flex-col gap-8 animate-fade-in absolute">
-              <div className="input_div">
-                <label htmlFor="title">Título</label>
-                <input
-                  id="title"
-                  name="title"
-                  placeholder="Lipps Inc. - Funkytown"
-                  value={data.title}
-                  onChange={(e) => setData({ ...data, title: e.target.value })}
-                />
-              </div>
-
-              <button type="submit" className="w-full mt-2 py-2 transition">
-                Descargar
-              </button>
+          <div className="w-full flex flex-col gap-8 animate-fade-in">
+            <div className="input_div">
+              <label htmlFor="title">Título</label>
+              <input
+                id="title"
+                name="title"
+                placeholder="Lipps Inc. - Funkytown"
+                value={data.title}
+                onChange={(e) => setData({ ...data, title: e.target.value })}
+              />
             </div>
+            <LiteYouTubeEmbed id={data.id} title="" />
+            <button type="submit" className="w-full mt-2 py-2 transition">
+              Descargar
+            </button>
           </div>
         )}
       </div>
